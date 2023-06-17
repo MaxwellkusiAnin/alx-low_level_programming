a024e402b7dd8e7297caa8f840eb31e5595e6698
MaxwellKusiAnin <MaxwellKusiAnin22@gmail.com>
2023-06-17 16:15:38 -0700

    keys

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - is a C code which output with the puts function
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+
+
+int main(void)
+{
+	puts("\ "programming is like building a multilingual puzzle");
+	return (0);
+}
+
