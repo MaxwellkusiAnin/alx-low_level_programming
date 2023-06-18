659bb7bf870a4f9426dfb326a4f00b7b44d5513b
MaxwellKusiAnin <MaxwellKusiAnin22@gmail.com>
2023-06-18 14:23:12 -0700

    Fixing

---
+++ 0x01-variables_if_else_while/kk.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, negative or zero
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <16; n++)
+	{
+		if (n <=9)
+		{
+			putchar('0' + n);
+		}
+		else
+		{
+			putchar(n + 87);
+		}
+	}
+	putchar('\n');
+	return (0);
+} 
